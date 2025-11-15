15db20ca8ad1bd16b29a50e723048c7e2f05e8be
Gilles TOURREAU <gilles.tourreau@pos-informatique.fr>
2025-11-15 16:21:21 +0100

    Fix warnings.

--- src/Text.Templating.Razor/IRazorTextTemplateRenderer.cs
+++ src/Text.Templating.Razor/IRazorTextTemplateRenderer.cs
@@ -6,8 +6,19 @@
 
 namespace PosInformatique.Foundations.Text.Templating.Razor
 {
+    /// <summary>
+    /// Used internaly by the <see cref="RazorTextTemplate{TModel}"/> to render a Razor component to a text output.
+    /// </summary>
     internal interface IRazorTextTemplateRenderer
     {
+        /// <summary>
+        /// Generates the text output of a Razor component.
+        /// </summary>
+        /// <param name="componentType">Type of the Razor component which will be use to generate the text.</param>
+        /// <param name="model">Model to inject in the Razor component.</param>
+        /// <param name="output">Output where the text will be generated.</param>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/> used to cancel the generation process.</param>
+        /// <returns>An instance of the <see cref="Task"/> which represents the asynchronous operation.</returns>
         Task RenderAsync(Type componentType, object? model, TextWriter output, CancellationToken cancellationToken = default);
     }
 }
